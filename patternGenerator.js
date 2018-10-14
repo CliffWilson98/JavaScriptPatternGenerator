8c1f0033202b30df2fc3dffa69aaa69b558c441d
Clifford Wilson <cswilso1@uno.edu>
2018-10-14 00:47:42 -0500

    beginning rough draft of GUI and buttons

--- patternGenerator.js
+++ patternGenerator.js
@@ -5,7 +5,7 @@ var canvasWidth = 500;
 var xVelocity = 2//Math.random() * 10;
 var yVelocity = 2//Math.random() * 10;
 //variables to hold the player objects
-var player, player2, player3, player4;
+var player, player2//, player3, player4;
 //variable to hold the canvas
 var canvas;
 
@@ -13,7 +13,7 @@ var canvas;
 //They will be referred to as Players.
 class Player
 {
-	//constructor must now x and y cordinates along with the height and width of object
+	//constructor must know x and y cordinates along with the height and width of object
 		constructor(x, y, width, height)
 		{
 			this.x = x;
@@ -31,8 +31,8 @@ function initializeData ()
 	canvas = document.getElementById("gameCanvas").getContext("2d");
 	player = new Player(0, 0, 32, 32);
 	player2 = new Player(468, 468, 32, 32);
-	player3 = new Player(468, 0, 32, 32);
-	player4 = new Player(0, 469, 32, 32);
+	//player3 = new Player(468, 0, 32, 32);
+	//player4 = new Player(0, 469, 32, 32);
 }
 
 function drawPlayer ()
@@ -42,10 +42,10 @@ function drawPlayer ()
 	canvas.fillRect(player.x, player.y, player.width, player.height);
 	canvas.fillStyle = "blue";
 	canvas.fillRect(player2.x, player2.y, player2.width, player2.height);
-	canvas.fillStyle = "red";
+	/*canvas.fillStyle = "red";
 	canvas.fillRect(player3.x, player3.y, player3.width, player3.height);
 	canvas.fillStyle = "green";
-	canvas.fillRect(player4.x, player4.y, player4.width, player4.height);
+	canvas.fillRect(player4.x, player4.y, player4.width, player4.height);*/
 }
 
 function drawBackground()
@@ -73,8 +73,8 @@ function gameLoop()
 	drawPlayer();
 	movePlayer(player);
 	movePlayer(player2);
-	movePlayer(player3);
-	movePlayer(player4);
+	//movePlayer(player3);
+	//movePlayer(player4);
 }
 
 	window.onload = function()
